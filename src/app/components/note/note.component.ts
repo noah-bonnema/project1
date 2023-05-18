59a0126e9903dd0f2f18bd60d12f85f0ed5c6464
noah.bonnema <noah.bonnema@hope.edu>
2023-05-18 13:22:51 -0400

    does this work

--- src/app/components/note/note.component.ts
+++ src/app/components/note/note.component.ts
@@ -45,11 +45,14 @@ export class NoteComponent {
 
     let j = 0;
     while (j <= this.notes.length) {
-      console.log("id: " + this.notes[j].noteId + " j: " + j);
-     
+      if(this.notes[j].noteId == n){
+        //this is the note to delete
+        this.notes.splice(n,1);
+        alert("note at number "+ n +" was removed");
+      }
       j++;
     }
-    console.log(j);
+   
 
   }
 
